2158bc102e0fa59da8c6cbec9c440fa50508bc67
boriscallens <Boris.Callens+github@gmail.com>
2009-11-21 13:38:03 +0000

    - Initial submit Nhaml.Utils

---
+++ Boris.Utils/Boris.Utils.NHaml/Helpers/HtmlHelperExtentions.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Boris.NHaml.Helpers
+{
+    public static class StyleSheetHelper
+    {
+        private const string styleSheetMask = 
+            @"<link rel='stylesheet' href='/content/css/{0}.css' type='text/css' media='{1}'/>";
+        public static HashSet<string> StyleSheets { get; set; }
+        public static void AddStyleSheet(string sheetName)
+        {
+            if(StyleSheets == null)
+            {
+                StyleSheets = new HashSet<string>();
+            }
+            StyleSheets.Add(sheetName);
+        }
+        public static string OutputStyleSheet()
+        {
+            return OutputStyleSheet("all");
+        }
+        public static string OutputStyleSheet(string mediaType)
+        {
+            if (StyleSheets == null || !StyleSheets.Any())
+                return string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var styleSheet in StyleSheets)
+            {
+                sb.AppendFormat(styleSheetMask, styleSheet, mediaType);
+            }
+            return sb.ToString();
+        }
+    }
+    public static class ScriptHelper
+    {
+        private const string scriptMask = @"<script src='/content/js/{0}.js' type='{1}'></script>";
+        public static HashSet<string> Scripts { get; set; }
+        public static void AddScript(string scriptName)
+        {
+            if (Scripts == null)
+            {
+                Scripts = new HashSet<string>();
+            }
+            Scripts.Add(scriptName);
+        }
+        public static string OutputScripts()
+        {
+            return OutputScripts("text/javascript");
+        }
+        public static string OutputScripts(string scriptType)
+        {
+            if (Scripts == null || !Scripts.Any())
+                return string.Empty;            
+            StringBuilder sb = new StringBuilder();
+            foreach (var script in Scripts)
+            {
+                sb.AppendFormat(scriptMask, script, scriptType);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Boris.Utils/Boris.Utils.NHaml/Rules/BuildupPlaceHolderRule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NHaml;
+using NHaml.Compilers;
+using NHaml.Rules;
+using NHaml.TemplateResolution;
+using System.IO;
+
+namespace Boris.NHaml.Rules
+{
+    public class BuildupPlaceHolderRule : MarkupRule
+    {
+        private const string NoPartialName = "No placeholder name specified";
+
+        public override string Signifier
+        {
+            get { return "?"; }
+        }
+
+        public override bool PerformCloseActions { get { return false; } }
+
+        public override BlockClosingAction Render(ViewSourceReader viewSourceReader, TemplateOptions options, TemplateClassBuilder builder)
+        {
+            var partialName = viewSourceReader.CurrentInputLine.NormalizedText.Trim();
+
+            if (string.IsNullOrEmpty(partialName))
+            {
+                if (viewSourceReader.ViewSourceQueue.Count == 0)
+                {
+                    throw new InvalidOperationException(NoPartialName);
+                }
+                var templatePath = viewSourceReader.ViewSourceQueue.Dequeue();
+                viewSourceReader.MergeTemplate(templatePath, true);
+            }
+            else
+            {
+                partialName = partialName.Insert(partialName.LastIndexOf(@"\", StringComparison.OrdinalIgnoreCase) + 1, "_");
+                IViewSource viewSource;
+                try
+                {
+                    viewSource = options.TemplateContentProvider.GetViewSource(partialName, viewSourceReader.ViewSources);
+                }
+                catch (FileNotFoundException)
+                {
+                    //ToDo: render content behind the ? tag
+                    viewSource = null;
+                }
+                viewSourceReader.ViewSourceModifiedChecks.Add(() => viewSource.IsModified);
+                viewSourceReader.MergeTemplate(viewSource, true);
+            }
+
+            return EmptyClosingAction;
+        }
+    }
+}
