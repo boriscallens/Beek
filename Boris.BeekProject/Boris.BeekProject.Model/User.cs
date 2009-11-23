e96a1f1e8f5b75742e4ba45bb3b31ac146103bc8
boriscallens <Boris.Callens+github@gmail.com>
2009-11-23 15:03:48 +0000

    - Testing out validation attributes on model classes - Cleaned out references - Added xVal, will first try out mvc2's validation system, but keeping the libraries and references in case the built in system fails

---
+++ Boris.BeekProject/Boris.BeekProject.Model/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Boris.BeekProject.Model
+{
+    public class User
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
