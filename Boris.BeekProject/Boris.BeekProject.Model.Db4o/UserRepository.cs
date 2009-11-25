d7d7ba393f81f21f634f062b68e5f98a7174bb9f
boriscallens <Boris.Callens+github@gmail.com>
2009-11-25 15:58:13 +0000

    - AddUser implementation

--- Boris.BeekProject/Boris.BeekProject.Model.Db4o/UserRepository.cs
+++ Boris.BeekProject/Boris.BeekProject.Model.Db4o/UserRepository.cs
@@ -1,20 +1,39 @@
 ﻿using System;
+using System.Configuration;
 using System.Linq;
 using Boris.BeekProject.Model.Accounts;
 using Boris.BeekProject.Model.DataAccess;
+using Db4objects.Db4o;
+using Db4objects.Db4o.Linq;
 
 namespace Boris.BeekProject.Model.Db4o
 {
     public class UserRepository: IUserRepository
     {
+        private static IObjectServer server = Db4oFactory.OpenServer(
+            ConfigurationManager.AppSettings["db4o.userRepository.path"], 0);
+        private static IObjectContainer client = Db4oFactory.OpenClient("localhost", 0, string.Empty, string.Empty);
+
         public Guid AddUser(IUser user)
         {
-            throw new NotImplementedException();
+            using (var db = GetDb())
+            {
+                Guid guid = new Guid();
+                user.Id = guid;
+                db.Store(user);
+                return guid;
+            }
         }
 
         public void RemoveUser(Guid id)
         {
-            throw new NotImplementedException();
+            using (var db = GetDb())
+            {
+                Guid guid = new Guid();
+                user.Id = guid;
+                db.Store(user);
+                return guid;
+            }
         }
 
         public IUser GetUserDataForUserName(string name)
@@ -40,6 +59,11 @@ namespace Boris.BeekProject.Model.Db4o
         public IQueryable<T> GetDefaultSettings<T>() where T : ISetting, new()
         {
             throw new NotImplementedException();
+        }
+    
+        private IObjectContainer GetDb()
+        {
+            return Db4oFactory.OpenFile();
         }
     }
 }
