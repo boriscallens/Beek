b2cf1f5c77f2a7e05d2e7aff1fe7a4dfc6fd5394
boriscallens <Boris.Callens+github@gmail.com>
2009-12-01 16:00:22 +0000

    - String.contains with Stringcomparoptions

---
+++ Boris.Utils/Boris.Utils.Strings/StringExtentions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Boris.Utils.Strings
+{
+    public static class StringExtentions
+    {
+        public static bool Contains(this string source, string toCheck, StringComparison comp)
+        {
+            return source.IndexOf(toCheck, comp) >= 0;
+        }
+    }
+}
