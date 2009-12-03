85486530db5d706c590eb7bc68fd87185fdb200e
boriscallens <Boris.Callens+github@gmail.com>
2009-12-03 09:51:08 +0000

    - For some reason I seemed to not add this one before. Strange

---
+++ Boris.BeekProject/Boris.BeekProject.Model/DataAccess/ICatalogRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Boris.BeekProject.Model.Beek;
+
+namespace Boris.BeekProject.Model.DataAccess
+{
+    public interface ICatalogRepository
+    {
+        IQueryable<Genre> GetGenres();
+        Boolean AddGenre(Genre g);
+        Boolean RemoveGenre(Genre g);
+
+        IQueryable<WritingStyle> GetWritingStyles();
+        Boolean AddWritingStyle(WritingStyle w);
+        Boolean RemoveWritingStyle(WritingStyle w);
+
+        IQueryable<BluePrint> GetBluePrints();
+
+        IQueryable<BaseBeek> GetBeek();
+        Boolean AddBeek(BaseBeek b);
+        Boolean RemoveBeek(BaseBeek b);
+    }
+}
